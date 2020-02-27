23585097a0b8e45d1e087f0e4e4b810f232cb90c
Kirby <kirby.forsberg@hotmail.com>
2020-02-27 18:57:51 -0500

    fixed sql code

--- Database/Database.sql
+++ Database/Database.sql
@@ -1,12 +1,20 @@
-CREATE TABLE Bird(
-bird_Id INTEGER  PRIMARY KEY AUTOINCREMENT,
-common_Name varchar (30) ,
-scientific_Name varchar (30) ,
+CREATE TABLE Entry(
+entry_Id INTEGER REFERENCES Bird(bird_Id),
+common_Name varchar (30),
+wiki_link varchar(255),
 conservation_Status varchar (25) ,
 eats int REFERENCES Feed_Mix(feed_Id),
 place_Seen int REFERENCES Location(location_Id),
 seen_Date DATETIME NOT NULL,
 seen_time DATETIME NOT NULL
+)
+
+CREATE TABLE Bird(
+bird_Id INTEGER PRIMARY KEY,
+common_Name varchar (30) ,
+scientific_Name varchar (30) ,
+conservation_Status varchar (25) ,
+eats int REFERENCES Feed_Mix(feed_Id),
 );
 
 CREATE TABLE Feed_Mix(
@@ -22,9 +30,3 @@ state varchar (20),
 weather_of int REFERENCES Weather(weather_Id)
 );
 
-CREATE TABLE Weather (
-weather_Id INTEGER PRIMARY KEY AUTOINCREMENT,
-percipitation BOOLEAN,
-temperature float,
-humidity float
-);
